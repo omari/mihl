7303f9a8e74ed45c2e34538c105e62c73af3d500
lo_gafet <lo_gafet@039de7f8-2a23-0410-abac-dce89ce9710f>
2007-07-07 03:39:08 +0000

    Added script and spec file to build RPM for the library.

---
+++ mihl/build-rpm-mihl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p /var/tmp/mihl/usr/lib
+cp Release/libmihl.so  /var/tmp/mihl/usr/lib
+strip /var/tmp/mihl/usr/lib/libmihl.so
+sudo rm -f /usr/src/redhat/RPMS/i386/mihl-*
+sudo touch /usr/src/redhat/RPMS/i386/mihl-0-2.i386.rpm
+sudo chmod a+rw /usr/src/redhat/RPMS/i386/mihl-0-2.i386.rpm
+
+rpmbuild -bb mihl.spec
