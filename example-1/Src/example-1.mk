2d304a05c8011a964331c54ac9e0bd51a3ffe942
lo_gafet <lo_gafet@039de7f8-2a23-0410-abac-dce89ce9710f>
2007-11-24 19:02:21 +0000

    Work in progress

--- example-1/Src/example-1.mk
+++ example-1/Src/example-1.mk
@@ -1,8 +1,9 @@
 include ../common.mk
 
 OBJS = example_1.o 
-TARGET = libmihl.so
-LIBS = -L ../mihl/$(CONFIG_NAME) -l mihl
+TARGET = example-1
+LIBS = -L ../../mihl/$(CONFIG_NAME) -l mihl
+CFLAGS += -I ../../mihl/Src
 CFLAGS += $(if $(filter 1 YES, $(DEBUG)), -g -O0, -g0 -O2)
 LDFLAGS += 
 include ../compile.mk	
